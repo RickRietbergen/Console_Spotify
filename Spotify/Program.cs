16246d27403ae1920798d58f2ff92757385c7a40
Rick Rietbergen <rbps3.2016@gmail.com>
2023-03-29 08:41:37 +0200

    Add project files.

---
+++ Spotify/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Spotiy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
