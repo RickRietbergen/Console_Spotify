7529962d244e0ac4e277fa42c53b2bd6597ef337
Rick Rietbergen <rbps3.2016@gmail.com>
2023-03-31 13:44:42 +0200

    play song, testing

--- Spotify/Program.cs
+++ Spotify/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NAudio.Wave;
 using System.Timers;
 using System.Media;
 
@@ -9,10 +10,25 @@ namespace Spotiy
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //List<string> playlist = new List<string>();
-            //playlist.Add(@"Songs/Josh Turner - Your Man.mp3");
-            //SoundPlayer player = new SoundPlayer("Songs/Josh Turner - Your Man.mp3");
-            //player.Play();
+            List<string> playlist = new List<string>();
+            //string[] files = Directory.GetFiles(@"C:\Users\rbps3\OneDrive - ROC Nijmegen\Bureaublad\schooljaar_2\C#\ConsoleSpotify\Spotify\Spotify\Songs\", "*.mp3");
+            //foreach (string file in files)
+            //{
+            //    playlist.Add(file);
+            //}
+            //foreach (string song in playlist)
+            //{
+            //    Console.WriteLine("Playing {0}", song);
+            //    // Code to play the song goes here
+            //}
+
+            string filePath = "C:\\Users\\rbps3\\OneDrive - ROC Nijmegen\\Bureaublad\\schooljaar_2\\C#\\ConsoleSpotify\\Spotify\\Spotify\\Songs\\Josh Turner - Your Man.mp3";
+            using (var player = new System.Media.SoundPlayer(filePath))
+            {
+                player.Play();
+                
+            }
+
             Console.Read();
         }
     }
